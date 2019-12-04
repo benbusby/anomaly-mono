31c46bdc7e0e36ba45c7e8d90ba62949e07d01bf
Ben Busby <33362396+benbusby@users.noreply.github.com>
2019-12-04 12:34:14 -0700

    Updated fontbakery script

--- fontbakery.sh
+++ fontbakery.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+bold=$(tput bold)
+red='\033[0;31m'
+green='\033[0;32m'
+ltcyan='\033[0;96m'
+normal=$(tput sgr0)
+nc='\033[0m'
+
+BREAK="\n=========================================\n"
+
 function runFontBakery () {
-    echo "Starting $1 (press Enter to continue)"
-    read -p ""
+    echo -e "${ltcyan}Running $1...${nc}"
+    sleep 1
     fontbakery $1 *.otf
+    case "$?" in
+        0)
+            echo -e "$BREAK"
+            echo -e "$1 : ${green}${bold}PASSED${normal}${nc}"
+            echo -e "$BREAK"
+            ;;
+        1)
+            echo -e "$BREAK"
+            echo -e "$1 : ${red}${bold}FAILED${normal}${nc}"
+            echo -e "$BREAK"
+            exit 1
+            ;;
+    esac
 }
 
 if ! [ -x "$(command -v fontbakery)" ]; then
